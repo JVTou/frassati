772828597a8bfdcdce4f17bd57434685ccdf4f7b
Junipero Verbeke <juniperoverbeke@gmail.com>
2025-09-24 13:23:00 -0700

    google analytics

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,10 @@
 const isPages = process.env.ELEVENTY_ENV === 'pages'
+const isDev = process.env.ELEVENTY_ENV === 'dev'
 
 export default {
   baseUrl: isPages
     ? '/11ty-landing-page/'
-    : '/'
+    : '/',
+  isDev,
+  analyticsId: process.env.GA_MEASUREMENT_ID || ''
 }
